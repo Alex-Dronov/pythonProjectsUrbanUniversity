6695f14c372cf5be2d2c27df72365cc4cfd6361c
Alex-Dronov <159939057+Alex-Dronov@users.noreply.github.com>
2024-09-03 11:50:53 +0300

    module_3_4 homework

--- module_3_4.py
+++ module_3_4.py
@@ -8,6 +8,7 @@ def single_root_words(root_word, *other_words):
 
         if (root_word in test_word) or (test_word in root_word):
             same_words.append(i)
+
     return same_words
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
