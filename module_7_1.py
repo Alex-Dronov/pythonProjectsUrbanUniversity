cc5f97406ab3934946892055cb19fbb78be44c5c
Alex-Dronov <159939057+Alex-Dronov@users.noreply.github.com>
2024-10-08 18:58:04 +0300

    module_7_1 homework

---
+++ module_7_1.py
@@ -0,0 +1,53 @@
+import os.path
+
+class Product:
+
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        prod_description = self.name + ', ' + self.weight.__str__() + ', ' + self.category
+        return prod_description
+
+
+class Shop:
+    __file_name = 'products.txt'
+
+    def get_products(self):
+        if os.path.isfile(self.__file_name):
+            products_file = open(self.__file_name, 'r')
+            products_str = products_file.read()
+            products_file.close()
+        else:
+            products_file = open(self.__file_name, 'a')
+            products_file.close()
+            products_str = ''
+
+        return products_str
+
+    def add(self, *products):
+
+        for product_ in products:
+
+            name_prod = product_.__str__()
+
+            if self.get_products().find(name_prod) != -1:
+                print(f'Продукт {name_prod} уже есть в магазине')
+            else:
+                products_file = open(self.__file_name, 'a')
+                products_file.write(name_prod + '\n')
+                products_file.close()
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
